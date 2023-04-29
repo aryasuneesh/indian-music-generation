cc7be76449f3b577da1909501c894f705f619a7c
aryasuneesh <aryasuneesh3@gmail.com>
2023-04-29 20:17:48 +0530

    Updated prepare_dataset.py

--- dataset/prepare_dataset.py
+++ dataset/prepare_dataset.py
@@ -35,20 +35,22 @@ def get_instrument_dict():
 
 # Open the CSV file
 def get_compiled():
-    
-with open('balanced_train_segments.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=' ')
-    for row in reader:
-        if row[0][0] == '#':
-            continue
-        video_id = row[0][0:-1]
-        start_time = int(row[1][:-5])
-        end_time = int(row[2][:-5])
-        video_link = f"https://www.youtube.com/watch?v={video_id}&t={start_time}s_{end_time}s"
-        instrument_id = row[3].split(",")
-        instrument_desc = [instrument_dict[id] for id in instrument_id if id in instrument_dict]
-        if instrument_desc:
-            compiled.append([video_link, instrument_desc, start_time, end_time, video_id])
+    instrument_dict = get_instrument_dict()
+    compiled = []
+    with open('balanced_train_segments.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=' ')
+        for row in reader:
+            if row[0][0] == '#':
+                continue
+            video_id = row[0][:-1]
+            start_time = int(row[1][:-5])
+            end_time = int(row[2][:-5])
+            video_link = f"https://www.youtube.com/watch?v={video_id}&t={start_time}s_{end_time}s" 
+            instrument_id = row[3].split(",")
+            instrument_desc = [instrument_dict[id] for id in instrument_id if id in instrument_dict]
+            if instrument_desc:
+                compiled.append([video_link, instrument_desc, start_time, end_time, video_id])
+    return compiled
 
 # Download audio from YouTube and prepare dataset
 sr = 22050
@@ -83,28 +85,36 @@ def preprocess_audio(audio_file: str) -> Tuple[np.ndarray, str]:
     
     return waveform, text
 
-dataset = []
+
 # Download audio and preprocess the dataset
-for data in compiled:
-    video_link, instrument_desc, start_time, end_time, video_id = data
-    filename = f"{video_id}.webm"
-    download_audio(video_link, start_time, end_time, audio_dir, "webm")
-    input_file = os.path.join(audio_dir, filename)
-    output_file = os.path.join(audio_dir, f"{video_id}.wav")
-    AudioSegment.from_file(input_file).export(output_file, format="wav")
-    os.remove(input_file)
-    wav, text = preprocess_audio(output_file)
-    for i in range(len(wav)):
-        dataset.append((wav[i], instrument_desc[i]))
+def get_dataset():
+    dataset = []
+    compiled = get_compiled()
+    for data in compiled:
+        video_link, instrument_desc, start_time, end_time, video_id = data
+        filename = f"{video_id}.webm"
+        download_audio(video_link, start_time, end_time, audio_dir, "webm")
+        input_file = os.path.join(audio_dir, filename)
+        output_file = os.path.join(audio_dir, f"{video_id}.wav")
+        AudioSegment.from_file(input_file).export(output_file, format="wav")
+        os.remove(input_file)
+        wav, text = preprocess_audio(output_file)
+        for i in range(len(wav)):
+            dataset.append((wav[i], instrument_desc[i]))
+    return dataset
+
 
 # Split dataset and create PyTorch dataloader
-np.random.shuffle(dataset)
-dataset_size = len(dataset)
-split = int(dataset_size * 0.8)  # use 80% of the data for training
+def create_dataloader():
+    dataset = get_dataset()
+    np.random.shuffle(dataset)
+    dataset_size = len(dataset)
+    split = int(dataset_size * 0.8)  # use 80% of the data for training
+    train_set = dataset[:split]
+    val_set = dataset[split:]
 
-train_set = dataset[:split]
-val_set = dataset[split:]
+    train_data = [(torch.from_numpy(wav), text) for (wav, text) in train_set]
+    val_data = [(torch.from_numpy(wav), text) for (wav, text) in val_set]
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=4, shuffle=True)
 
-train_data = [(torch.from_numpy(wav), text) for (wav, text) in train_set]
-val_data = [(torch.from_numpy(wav), text) for (wav, text) in val_set]
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=4, shuffle=True)
+    return train_loader, val_data, train_data
